149527bf7167d23077d0f1e8c1f4051175c82d3c
Betemariam <betemariam216rfa@gmail.com>
2021-08-28 11:40:35 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+int main()
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
