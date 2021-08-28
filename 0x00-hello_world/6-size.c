5ed948b836dbb1e89d38e0abe82f8c88826416e6
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-08-28 07:10:22 -0400

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,19 +3,19 @@
  * main - This is a description
  * Return: int will be returned
  */
-int main()
+int main(void)
 {
-  int intType;
-  float floatType;
-  long int longint;
-  long long int longlongint;
-  double doubleType;
-  char charType;
+int intType;
+float floatType;
+long int longint;
+long long int longlongint;
+double doubleType;
+char charType;
 
-  printf("Size of char: %zu byte\n", sizeof(charType));
-  printf("Size of int: %zu bytes\n", sizeof(intType));
-  printf("Size of long int: %zu bytes\n", sizeof(longint));
-  printf("Size of long long int: %zu bytes\n", sizeof(longlongint));
-  printf("Size of float: %zu bytes\n", sizeof(floatType));
-  return 0;
+printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of long int: %zu bytes\n", sizeof(longint));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongint));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+return (0);
 }
