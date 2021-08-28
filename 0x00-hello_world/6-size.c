8ea4939e381283e04695953c22108660ee50e0ab
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-08-28 07:13:19 -0400

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ double doubleType;
 char charType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
-printf("Size of a long int: %zu bytes(s)\n", sizeof(longint));
-printf("Size of a long long int: %zu bytes(s)\n", sizeof(longlongint));
-printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongint));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
