7929a7ca88c5dc9112969633c68028a6e81681d3
Betemariam <betemariam216rfa@gmail.com>
2021-08-29 20:29:25 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+int main(void)
+{
+  char i;
+  for(i = '0'; i <= '9' ; i++){
+    putchar(i);
+  }
+  for(i = 'a'; i <= 'f' ; i++){
+    putchar(i);
+  }
+  putchar('\n');
+  return (0);
+}
