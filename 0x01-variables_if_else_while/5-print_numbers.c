82821644e8ba18b78e3238c6b60b301f79ee29c6
Betemariam <betemariam216rfa@gmail.com>
2021-08-29 18:54:33 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+int main(void)
+{
+  int a;
+
+  for (a = 0; a < 10; a++)
+    {
+      printf("%d",a);
+    }
+  return (0);
+}
