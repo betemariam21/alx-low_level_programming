a2030d6c312b15368caf6639545d5bfef5af4cf8
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-08-29 15:30:40 -0400

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,9 @@
  */
 int main(void)
 {
-
-  for(int i = 0; i < 10 ; i++){
-    putchar((i%10)+'0');
-  }
-  printf("\n");
-  return (0);
+for(int i = 0; i < 10 ; i++){
+putchar((i%10)+'0');
+}
+printf("\n");
+return (0);
 }
