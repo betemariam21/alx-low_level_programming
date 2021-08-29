e6a071a0609a6a333d6bf81d3a928ce36b2f5191
Betemariam <betemariam216rfa@gmail.com>
2021-08-29 14:45:53 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+int main(void)
+{
+  char be='a' ;
+  char bee = '\n';
+
+  for(be == 'a'; be<='z' ;be++)
+    {
+
+      if(be != 'e' && be != 'q')
+	putchar(be);
+    }
+  putchar(bee);
+  return (0);
+}
