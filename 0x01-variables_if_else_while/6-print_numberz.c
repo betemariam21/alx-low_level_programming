9db120c0c37b39bf5f38ba79bf0e1efbdc328212
Betemariam <betemariam216rfa@gmail.com>
2021-08-29 19:28:18 +0000

    #include <stdio.h> /**  * main - This is a description  * Return: int will be returned  */ int main(void) {
    for(int i = 0; i < 10 ; i++){
    putchar((i%10)+'0');
    }
    printf(n);
    return (0);
    }

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+int main(void)
+{
+
+  for(int i = 0; i < 10 ; i++){
+    putchar((i%10)+'0');
+  }
+  printf("\n");
+  return (0);
+}
