9c14f9b672777335d239f0403ab711655b3d85f0
Betemariam <betemariam216rfa@gmail.com>
2021-08-29 19:52:10 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: int will be returned
+ */
+#include<stdio.h>
+int main()
+{
+  char i;
+  for(i='z';i>='a';i--)
+    {
+      putchar(i);
+    }
+  putchar('\n');
+  return 0;
+}
