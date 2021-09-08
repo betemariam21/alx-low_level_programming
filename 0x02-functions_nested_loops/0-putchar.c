0017184a266240ac9a6a9f76e3b5ce0626c4aacf
Betemariam <betemariam216rfa@gmail.com>
2021-09-08 16:12:57 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "_putchar.h"
+
+int main(void){
+  char a='_';
+  char b='p';
+  char c='u';
+  char d='t';
+  char e='c';
+  char f='h';
+  char g='a';
+  char h='r';
+
+  _putchar(a);
+  _putchar(b);
+  _putchar(c);
+  _putchar(d);
+  _putchar(e);
+  _putchar(f);
+  _putchar(g);
+  _putchar(h);
+  return (0);
+}
