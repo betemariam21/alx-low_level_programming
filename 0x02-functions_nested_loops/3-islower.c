c438120b025ecd0707595328f44a77c295b2dfaa
Betemariam <betemariam216rfa@gmail.com>
2021-09-09 01:10:08 +0000

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ *
+ * Return: void.
+ */
+int _islower(int c)
+{
+  if (c>64 && c<91)
+    {
+      return (0);
+    }
+  else
+    {
+      return (1);
+    }
+}
