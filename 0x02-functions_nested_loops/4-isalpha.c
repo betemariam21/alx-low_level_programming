1b848b407414bb38bf90e6129fb579669004d05a
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-08 21:43:44 -0400

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,22 @@
 #include "main.h"
-
+/**
+ * _islower - check the code
+ *
+ * Return: int.
+ * @c: First operand
+ */
 int _isalpha(int c)
 {
-  if (c>64 && c<91)
-    {
-      return (1);
-    }
-  else if (c > 97 && c < 122)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else if (c > 97 && c < 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
