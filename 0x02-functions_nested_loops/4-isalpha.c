f51c2c033181cfd5ef092284c659929936a6f7da
Betemariam <betemariam216rfa@gmail.com>
2021-09-09 01:36:22 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int _isalpha(int c)
+{
+  if (c>64 && c<91)
+    {
+      return (1);
+    }
+  else if (c > 97 && c < 122)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
