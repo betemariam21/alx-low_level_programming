5e56b7cbfe25c7220ca90bfaf67d7a55fcf307c7
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-09 15:01:13 -0400

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-void jack_bauer(void){
-  int h,m;
-  for(h=0;h<24;h++){
-    for(m=0;m<60;m++){
-	printf("%02d:%02d",h,m);
-	printf("\n");
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+void jack_bauer(void)
+{
+int h,m;
 	
-    }
-  }
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+printf("%02d:%02d",h,m);
+printf("\n");
 }
-
-  
+}
+}
