186bcb124a4818bedc6626d294feed5399675b2c
Betemariam <betemariam216rfa@gmail.com>
2021-09-09 18:43:45 +0000

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+void jack_bauer(void){
+  int h,m,s;
+  for(h=0;h<24;h++){
+    for(m=0;m<60;m++){
+      for(s=0;s<60;s++){
+	printf("%d:%d",h,s);
+	printf("\n");
+
+
+      }
+
+    }
+  }
+}
+
+  
