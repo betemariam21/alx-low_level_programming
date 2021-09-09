03dd011d2579410c9ffd828277ec7755ee5b8854
Betemariam <betemariam216rfa@gmail.com>
2021-09-09 00:42:48 +0000

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+  char ch;
+  int i;
+  for(i=0;i<=9;i++){
+
+    for (ch = 'a'; ch <= 'z'; ch++){
+
+      _putchar(ch);
+    }
+    _putchar('\n');
+  }
+}
