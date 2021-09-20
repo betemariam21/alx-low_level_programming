f4c53a45374fe8323c1324c443877be8edb43355
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-20 16:59:58 -0400

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
+/**
+ * _isdigit - check the code
+ *
+ * Return: int.
+ * @c: First operand
+ */
 int _isdigit(int c)
 {
-  if (c >= '0' && c <= '9')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
-
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
