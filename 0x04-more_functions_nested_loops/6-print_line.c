5f8d3fce0063d1cc7d9dffb253f9a7072fbfae3a
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-20 18:54:03 -0400

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,17 @@
 #include "main.h"
+/**
+ * print_line - check the code
+ *
+ * Return: void.
+ *@n : arg1
+ */
+void print_line(int n)
+{
+int i;
 
-void print_line(int n){
-  int i;
-  for(i=1;i<=n;i++){
-    _putchar('_');
-  }
-  _putchar('\n');
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
