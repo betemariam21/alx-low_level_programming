5868a13e19a7ac230e5e1297175c4e81e1332aac
Betemariam <betemariam216rfa@gmail.com>
2021-09-20 23:27:12 +0000

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+  int i, j;
+  if (n > 0)
+    {
+      for (i = 0; i < n; i++)
+	{
+	  for (j = 0; j <= i; j++)
+	    {
+	      _putchar(' ');
+	      if (j == i)
+		{
+		  _putchar('\\');
+		}
+	    }
+	  _putchar('\n');
+	}
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
