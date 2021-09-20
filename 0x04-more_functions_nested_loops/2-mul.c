1dde49389ebc3f844145cfd6d0987ccf91745fb4
Betemariam <betemariam216rfa@gmail.com>
2021-09-20 21:22:59 +0000

    3-main.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,14 @@
 #include "main.h"
+/**
+ * mul - check the code
+ *
+ * Return: int.
+ * @a: First operand
+ * @b: second operand
+ */
+int mul(int a, int b)
+{
+  int result = a * b;
 
-int mul(int a, int b){
-  int result = a*b;
   return result;
 }
