9804421ff385fdab3fa469fa2483f38c698002b9
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-20 16:36:03 -0400

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
-int _isupper(int c)
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return: always(0)
+ */
+int _isupper(int c) 
 {
-if(c >= 65 &&  c <= 90 ){
-
+if(c >= 65 && c <= 90 )
+{
 return (1);
-
 }
 else
 {
