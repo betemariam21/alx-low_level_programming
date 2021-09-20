0ed662bbc5dc4213fb6151dfe586d35106554140
betemariam21 <88780947+betemariam21@users.noreply.github.com>
2021-09-20 17:54:13 -0400

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: void.
+ */
 void print_most_numbers(void)
 {
-  int a;
-  for(a = 0;a <= 9;a++)
-    {
-      if(a !=2 && a != 4)
-	_putchar('0'+a);
-    }
-  _putchar('\n');
+int a;
+  
+for (a = 0; a <= 9; a++)
+{
+if (a !=2 && a != 4)
+_putchar('0' + a);
+}
+_putchar('\n');
 }
