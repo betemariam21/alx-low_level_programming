810a92af1aed3bcfb17bc2ce5590e0945b7c7658
Betemariam <betemariam216rfa@gmail.com>
2021-09-20 20:45:46 +0000

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
-int _isupper(int c){
-  if (c == 65){
-
-  return (1);
-
-  }
- else
- {
-
- return (0);
- } 
- }
+/**
+ * _isupper - check the code
+ *
+ * Return: int.
+ * @c: First operand
+ */
+int _isupper(int c)
+{
+  if (c >= 65 && c <= 90)
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+}
